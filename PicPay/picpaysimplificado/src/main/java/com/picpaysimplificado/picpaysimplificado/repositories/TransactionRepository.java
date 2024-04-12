0aaa652c03f512bce1cf263687cba39b22996eb2
Yan Witer <yanwiter@gmail.com>
2024-04-12 05:50:32 -0300

    Alteração nos repositórios

--- PicPay/picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/TransactionRepository.java
+++ PicPay/picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/TransactionRepository.java
@@ -1,11 +1,11 @@
 package com.picpaysimplificado.picpaysimplificado.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.picpaysimplificado.picpaysimplificado.domain.transaction.Transaction;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-
-    
 
 }
--- PicPay/picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/UserRepository.java
+++ PicPay/picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/repositories/UserRepository.java
@@ -1,15 +1,15 @@
 package com.picpaysimplificado.picpaysimplificado.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.picpaysimplificado.picpaysimplificado.domain.user.User;
 import java.util.Optional;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByDocument(String document);
-
     Optional<User> findUserById(Long id);
 
 }
